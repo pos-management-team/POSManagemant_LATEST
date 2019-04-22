cd2a696ec3d26d3f5e4410b0dc63581708705460
Mohammad Sadikur Rahman <32734077+rsadikur66@users.noreply.github.com>
2019-04-23 01:09:40 +0600

    22 april

---
+++ POSManagemant/Controllers/Setups/T12003Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace POSManagemant.Controllers.Setups
+{
+    public class T12003Controller : Controller
+    {
+        // GET: T12003
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
